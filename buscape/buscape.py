19017eed9fb091b57d4784718d6563271f306fea
Fernando G N Rocha <fernandogrd@gmail.com>
2011-09-03 23:45:15 -0300

    PEP8

--- buscape/buscape.py
+++ buscape/buscape.py
@@ -1,19 +1,20 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-__author__="Igor Hercowitz"
-__author__="Alê Borba"
-__version__="v0.6.1"
+__author__ = "Igor Hercowitz"
+__author__ = "Alê Borba"
+__version__ = "v0.6.1"
 
 from urllib import urlencode
-from urllib2 import urlopen, Request, URLError, HTTPError
+from urllib2 import urlopen, URLError, HTTPError
+
 
 class Buscape():
     """
     Class for BuscaPé's API abstraction
     """
 
-    def __init__(self,applicationID=None, country="BR"):
+    def __init__(self, applicationID=None, country="BR"):
         if not applicationID:
             raise ValueError("User ID must be specified")
 
@@ -26,18 +27,19 @@ class Buscape():
         else:
             self.country = country
 
-
     def __fetch_url(self, url=None):
         try:
             resp = urlopen(url)
             data = resp.read()
-            return dict(code=resp.code,data=data)
+            return dict(code=resp.code, data=data)
         except HTTPError, e:
             if e.code == 401:
                 if self.environment == 'bws':
-                    raise HTTPError(url,e.code,"Your application is not approved yet",None,None)
+                    raise HTTPError(url, e.code, "Your application is not "
+                                    "approved yet", None, None)
                 else:
-                    raise HTTPError(url,e.code,"The request requires user authentication",None,None)
+                    raise HTTPError(url, e.code, "The request requires user "
+                                    "authentication", None, None)
         except URLError, e:
             code_error = e.reason.errno
 
@@ -46,21 +48,21 @@ class Buscape():
             else:
                 raise URLError(e)
 
-
     def __search(self, method=None, parameter=None):
         if self.environment != 'sandbox':
             self.environment = 'bws'
 
-        req = "http://%s.buscape.com/service/%s/%s/%s/?%s" %(self.environment, method, self.applicationID, self.country, parameter)
+        req = "http://%s.buscape.com/service/%s/%s/%s/?%s" %\
+              (self.environment, method, self.applicationID, self.country,
+               parameter)
 
         try:
             ret = self.__fetch_url(url=req)
             return ret
-        except HTTPError,e:
+        except HTTPError, e:
             raise e
         except URLError, e:
             raise e
-
 
     def set_sandbox(self):
         """
@@ -68,52 +70,51 @@ class Buscape():
         """
         self.environment = 'sandbox'
 
-
     def find_category_list(self, keyword=None, categoryID=None, format='XML'):
         """
         Método faz busca de categorias, permite que você exiba informações
-        relativas às categorias. É possível obter categorias, produtos ou ofertas
-        informando apenas um ID de categoria.
+        relativas às categorias. É possível obter categorias, produtos ou
+        ofertas informando apenas um ID de categoria.
         """
 
-        if format.upper() not in ["XML","JSON"]:
+        if format.upper() not in ["XML", "JSON"]:
             raise ValueError("the return format must be XML or JSON")
 
-        if not keyword and (categoryID < 0 or categoryID is None or categoryID==''):
+        if not keyword and (categoryID < 0 or not categoryID):
             raise ValueError("keyword or categoryID option must be specified")
         elif keyword and categoryID:
-            raise ValueError("you must specify only keyword or categoryID. Both values aren't accepted")
+            raise ValueError("you must specify only keyword or categoryID. "
+                             "Both values aren't accepted")
 
         if keyword:
-            parameter = "keyword=%s" %keyword
+            parameter = "keyword=%s" % keyword
         else:
-            parameter = "categoryId=%s" %categoryID
+            parameter = "categoryId=%s" % categoryID
 
-        parameter = parameter + "&format=%s" %(format)
-
+        parameter = parameter + "&format=%s" % (format)
 
         ret = self.__search(method='findCategoryList', parameter=parameter)
         return ret
 
-
-    def find_product_list(self, keyword=None, categoryID=None, format='XML', lomadee=False, results=10,
-                            page=1, minPrice=0.0, maxPrice=0.0):
+    def find_product_list(self, keyword=None, categoryID=None, format='XML',
+                          lomadee=False, results=10, page=1, minPrice=0.0,
+                          maxPrice=0.0):
         """
         Método permite que você busque uma lista de produtos únicos
         utilizando o id da categoria final ou um conjunto de palavras-chaves
         ou ambos.
         """
 
-        if format.upper() not in ["XML","JSON"]:
+        if format.upper() not in ["XML", "JSON"]:
             raise ValueError("the return format must be XML or JSON")
 
-        if not keyword and (categoryID < 0 or categoryID is None or categoryID==''):
+        if not keyword and (categoryID < 0 or not categoryID):
             raise ValueError("keyword or categoryID option must be specified")
 
-        if results not in range(1,999):
+        if results not in range(1, 999):
             raise ValueError("results must be a integer between 1 and 999")
 
-        if page not in range(1,999):
+        if page not in range(1, 999):
             raise ValueError("page number must be a integer between 1 and 999")
 
         if minPrice is None or minPrice == '':
@@ -126,14 +127,15 @@ class Buscape():
         elif maxPrice < 0.0:
             raise ValueError("maximum price can not be negative")
         elif maxPrice > 0.0 and minPrice > maxPrice:
-            raise ValueError("minimum price can not be greater than maximum price")
+            raise ValueError("minimum price can not be greater than maximum "
+                             "price")
 
         params = {'format': format, 'results': results, 'page': page}
 
         if keyword:
             params['keyword'] = keyword
         if categoryID:
-            params['categoryID']  = categoryID
+            params['categoryID'] = categoryID
         if maxPrice:
             params['maxPrice'] = maxPrice
         if minPrice:
@@ -148,21 +150,20 @@ class Buscape():
 
         return self.__search(method=method, parameter=parameter)
 
-
-
-    def create_source_id(self, sourceName=None, publisherID=None, siteID=None, campaignList=None, token=None, format='XML'):
+    def create_source_id(self, sourceName=None, publisherID=None, siteID=None,
+                         campaignList=None, token=None, format='XML'):
         """
         Serviço utilizado somente na integração do Aplicativo com o Lomadee.
-        Dentro do fluxo de integração, o aplicativo utiliza esse serviço para
-        criar sourceId (código) para o Publisher que deseja utiliza-lo.
-        Os parâmetros necessários neste serviço são informados pelo próprio
-        Lomadee ao aplicativo.
-        No ambiente de homologação sandbox, os valores dos parâmetros podem ser
-        fictícios pois neste ambiente este serviço retornará sempre o mesmo sourceId
-        para os testes do Developer.
+        Dentro do fluxo de integração, o aplicativo utiliza esse serviço
+        para criar sourceId (código) para o Publisher que deseja utiliza-lo.
+        Os parâmetros necessários neste serviço são informados pelo
+        próprio Lomadee ao aplicativo.
+        No ambiente de homologação sandbox, os valores dos parâmetros podem
+        ser fictícios pois neste ambiente este serviço retornará sempre o
+        mesmo sourceId para os testes do Developer.
         """
 
-        if format.upper() not in ["XML","JSON"]:
+        if format.upper() not in ["XML", "JSON"]:
             raise ValueError("the return format must be XML or JSON")
 
         if sourceName is None:
@@ -178,24 +179,29 @@ class Buscape():
             raise ValueError("token option must be specified")
 
         if campaignList:
-            parameter = "sourceName=%s&publisherId=%s&siteId=%s&campaignList=%s&token=%s&format=%s" %(sourceName,publisherID,siteID,campaignList,token,format)
+            parameter = "sourceName=%s&publisherId=%s&siteId=%s&"
+            "campaignList=%s&token=%s&format=%s" %\
+                        (sourceName, publisherID, siteID, campaignList, token,
+                         format)
         else:
-            parameter = "sourceName=%s&publisherId=%s&siteId=%s&token=%s&format=%s" %(sourceName,publisherID,siteID,token,format)
+            parameter = "sourceName=%s&publisherId=%s&siteId=%s&token=%s&"
+            "format=%s" % (sourceName, publisherID, siteID, token, format)
 
         ret = self.__search(method='createSource/lomadee', parameter=parameter)
 
         return ret
 
-
-    def find_offer_list(self, categoryID=None, productID=None, barcode=None, keyword=None, lomadee=False, format="XML",
-                        results=None, page=None, priceMin=None, priceMax=None, sort=None, medal=None):
+    def find_offer_list(self, categoryID=None, productID=None, barcode=None,
+                        keyword=None, lomadee=False, format="XML",
+                        results=None, page=None, priceMin=None, priceMax=None,
+                        sort=None, medal=None):
         """
         Método permite que você busque uma lista de produtos únicos
         utilizando o id da categoria final ou um conjunto de palavras-chaves
         ou ambos.
         """
 
-        if format.upper() not in ["XML","JSON"]:
+        if format.upper() not in ["XML", "JSON"]:
             raise ValueError("the return format must be XML or JSON")
 
         if lomadee:
@@ -203,78 +209,81 @@ class Buscape():
         else:
             method = 'findOfferList'
 
-        if categoryID >=0 and keyword:
-            parameter = "categoryId=%s&keyword=%s" %(categoryID,keyword)
-        elif categoryID >=0:
-            parameter = "categoryId=%s" %(categoryID)
+        if categoryID >= 0 and keyword:
+            parameter = "categoryId=%s&keyword=%s" % (categoryID, keyword)
+        elif categoryID >= 0:
+            parameter = "categoryId=%s" % (categoryID)
         elif keyword:
-            parameter = "keyword=%s" %(keyword)
+            parameter = "keyword=%s" % (keyword)
         elif barcode:
-            parameter = "barcode=%s" %(barcode)
+            parameter = "barcode=%s" % (barcode)
         elif productID:
-            parameter = "productId=%s" %(productID)
+            parameter = "productId=%s" % (productID)
         else:
             raise ValueError("One parameter must be especified")
 
         #Montando o filtro
         if results:
-            parameter = parameter + "&results=%s" %(results)
+            parameter = parameter + "&results=%s" % (results)
 
         if page:
-            parameter = parameter + "&page=%s" %(page)
+            parameter = parameter + "&page=%s" % (page)
 
         if priceMin:
-            parameter = parameter + "&priceMin=%s" %(priceMin)
+            parameter = parameter + "&priceMin=%s" % (priceMin)
 
         if priceMax:
-            parameter = parameter + "&priceMax=%s" %(priceMax)
+            parameter = parameter + "&priceMax=%s" % (priceMax)
 
         if sort:
-            if sort in ['price','dprice','rate','drate','seller','dseller','installment','dinstallment','numberofinstallments','dnumberofinstallments','trustedStore']:
-                parameter = parameter + "&sort=%s" %(sort)
+            if sort in ['price', 'dprice', 'rate', 'drate', 'seller',
+                        'dseller', 'installment', 'dinstallment',
+                        'numberofinstallments', 'dnumberofinstallments',
+                        'trustedStore']:
+                parameter = parameter + "&sort=%s" % (sort)
             else:
-                raise ValueError('The value in the sort parameter is not valid')
+                raise ValueError('The value in the sort parameter is not '
+                                 'valid')
 
         if medal:
-            if medal in ['all','diamond','gold','silver','bronze']:
-                parameter = parameter + "medal=%s" %(medal)
+            if medal in ['all', 'diamond', 'gold', 'silver', 'bronze']:
+                parameter = parameter + "medal=%s" % (medal)
             else:
-                raise ValueError('The value in the medal parameter is not valid')
+                raise ValueError('The value in the medal parameter is not '
+                                 'valid')
 
-        parameter = parameter+"&format=%s" %(format)
+        parameter = parameter + "&format=%s" % (format)
 
         ret = self.__search(method=method, parameter=parameter)
 
         return ret
-
 
     def top_products(self, format="XML", filterID=None, valueID=None):
         """
         Método que retorna os produtos mais populares do BuscaPé.
         """
 
-        if format.upper() not in ["XML","JSON"]:
+        if format.upper() not in ["XML", "JSON"]:
             raise ValueError("the return format must be XML or JSON")
 
         method = "topProducts"
 
-        parameter = "format=%s" %(format)
+        parameter = "format=%s" % (format)
 
         #Montando o filtro
         if filterID and valueID:
-            parameter = parameter + "&specId%s=%s" %(filterID,valueID)
+            parameter = parameter + "&specId%s=%s" % (filterID, valueID)
 
-        ret = self.__search(method=method,parameter=parameter)
+        ret = self.__search(method=method, parameter=parameter)
 
         return ret
 
-
-    def view_product_details(self, productID=None,format="XML"):
+    def view_product_details(self, productID=None, format="XML"):
         """
         Método retorna os detalhes técnicos de um determinado produto.
         """
 
-        if format.upper() not in ["XML","JSON"]:
+        if format.upper() not in ["XML", "JSON"]:
             raise ValueError("the return format must be XML or JSON")
 
         if not productID:
@@ -282,19 +291,19 @@ class Buscape():
 
         method = "viewProductDetails"
 
-        parameter = "productId=%s&format=%s" %(productID,format)
+        parameter = "productId=%s&format=%s" % (productID, format)
 
-        ret = self.__search(method=method,parameter=parameter)
+        ret = self.__search(method=method, parameter=parameter)
 
         return ret
 
-
-    def view_seller_details(self,sellerID=None,format="XML"):
+    def view_seller_details(self, sellerID=None, format="XML"):
         """
-        Método que retorna os detalhes de uma loja ou empresa como: endereços, telefones de contato e etc.
+        Método que retorna os detalhes de uma loja ou empresa como:
+        endereços, telefones de contato e etc.
         """
 
-        if format.upper() not in ["XML","JSON"]:
+        if format.upper() not in ["XML", "JSON"]:
             raise ValueError("the return format must be XML or JSON")
 
         if not sellerID:
@@ -302,19 +311,19 @@ class Buscape():
 
         method = "viewSellerDetails"
 
-        parameter = "sellerId=%s&format=%s" %(sellerID,format)
+        parameter = "sellerId=%s&format=%s" % (sellerID, format)
 
-        ret = self.__search(method=method,parameter=parameter)
+        ret = self.__search(method=method, parameter=parameter)
 
         return ret
 
-
     def view_user_ratings(self, productID=None, format="XML"):
         """
-        Método que retorna as avaliações dos usuários sobre um determinado produto.
+        Método que retorna as avaliações dos usuários sobre um determinado
+        produto.
         """
 
-        if format.upper() not in ["XML","JSON"]:
+        if format.upper() not in ["XML", "JSON"]:
             raise ValueError("the return format must be XML or JSON")
 
         if not productID:
@@ -322,8 +331,8 @@ class Buscape():
 
         method = "viewUserRatings"
 
-        parameter = "productId=%s&format=%s" %(productID,format)
+        parameter = "productId=%s&format=%s" % (productID, format)
 
-        ret = self.__search(method=method,parameter=parameter)
+        ret = self.__search(method=method, parameter=parameter)
 
         return ret
